e673ab8127025fa9a8f6689164369485cb647a42
t.novotny <novotny.tom99@gmail.com>
2021-04-11 19:53:02 +0200

    Cleaned branch code

--- CSharpCourse/Program.cs
+++ CSharpCourse/Program.cs
@@ -3,24 +3,7 @@
 namespace CSharpCourse {
     class Program {
         static void Main(string[] args) {
-            int integer = 12;
-            Console.WriteLine(integer);
-
-            float aFloat = 12.2F;
-            Console.WriteLine(aFloat);
-
-            bool aBool = true;
-            Console.WriteLine(aBool);
-
-            aBool = aFloat > integer;
-            Console.WriteLine(aBool);
-
-            Console.WriteLine("Write something and I will repeat it twice:");
-            string input;
-            input = Console.ReadLine();
-            string output = $"{input}, {input}!";
-            Console.WriteLine(output);
-            Console.ReadKey();
+            
         }
     }
 }

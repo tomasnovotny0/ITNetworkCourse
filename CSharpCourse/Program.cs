b70e714ccdb4a185ef0c739979ecae5857c84154
t.novotny <novotny.tom99@gmail.com>
2021-04-11 19:20:35 +0200

    Basic variables

--- CSharpCourse/Program.cs
+++ CSharpCourse/Program.cs
@@ -3,7 +3,18 @@
 namespace CSharpCourse {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello world");
+            int integer = 12;
+            Console.WriteLine(integer);
+
+            float aFloat = 12.2F;
+            Console.WriteLine(aFloat);
+
+            bool aBool = true;
+            Console.WriteLine(aBool);
+
+            aBool = aFloat > integer;
+            Console.WriteLine(aBool);
+            
             Console.ReadKey();
         }
     }

7f27c43678756fe9073ebda56b0ff30dd41b5fc2
t.novotny <novotny.tom99@gmail.com>
2021-04-16 11:38:58 +0200

    Cvičení 7

--- CSharpCourse/Program.cs
+++ CSharpCourse/Program.cs
@@ -1,133 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace CSharpCourse {
     class Program {
         static void Main(string[] args) {
-            Hra hra = new Hra();
-            while (hra.Bezi()) {
-                Console.Write("Zadej příkaz: ");
-                string prikaz = Console.ReadLine();
-                hra.Akce(prikaz);
+            Rodokmen rodokmen = new Rodokmen();
+            Console.WriteLine("Rodokmen pro osobu {0}", rodokmen.bartSimpson);
+            rodokmen.VypisOsobu(rodokmen.bartSimpson);
+            Console.WriteLine();
+            Console.WriteLine("Rodokmen pro osobu {0}", rodokmen.homerSimpson);
+            rodokmen.VypisOsobu(rodokmen.homerSimpson);
+        }
+    }
+
+    class Rodokmen {
+
+        public readonly Osoba homerSimpson;
+        public readonly Osoba bartSimpson;
+        
+        public Rodokmen() {
+            Osoba abrahamS = new Osoba("Abraham Simpson");
+            Osoba penelopeO = new Osoba("Penelope Olsen");
+            Osoba herbP = new Osoba("Herb Powers");
+            homerSimpson = new Osoba("Homer Simpson");
+            herbP.NastavRodice(abrahamS, penelopeO);
+            homerSimpson.NastavRodice(abrahamS, penelopeO);
+            Osoba panB = new Osoba("Pan Bouvier");
+            Osoba jackieB = new Osoba("Jackie Bouvier");
+            Osoba margeB = new Osoba("Marge Bouvier");
+            Osoba selmaB = new Osoba("Selma Bouvier");
+            margeB.NastavRodice(panB, jackieB);
+            selmaB.NastavRodice(panB, jackieB);
+            bartSimpson = new Osoba("Bart Simpson");
+            bartSimpson.NastavRodice(homerSimpson, margeB);
+        }
+
+        public void VypisOsobu(Osoba osoba) {
+            if (osoba != null) {
+                Console.WriteLine(osoba);
+                VypisOsobu(osoba.VratOtce());
+                VypisOsobu(osoba.VratMatku());
             }
         }
     }
 
-    class Hra {
-        public readonly Lokace hrad = new Lokace("Hrad", "Stojíš před okovanou branou gotického hradu, která je zřejmě jediným vchodem do pevnosti. Klíčová dírka je pokryta pavučinami, což vzbuzuje dojem, že je budova opuštěná.");
-        public readonly Lokace lesHradRozcesti = new Lokace("Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce. Ticho podvečerního lesa občas přeruší zpěv posledních ptáků.");
-        public readonly Lokace lesniRozcesti = new Lokace("Lesní rozcestí", "Nacházíš se na lesním rozcestí.");
-        public readonly Lokace lesRozcestiRybnik = new Lokace("Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce. Ticho podvečerního lesa občas přeruší zpěv posledních ptáků.");
-        public readonly Lokace rybnik = new Lokace("Rybník", "Došel jsi ke břehu malého rybníka. Hladina je v bezvětří jako zrcadlo. Kousek od tebe je dřevěná plošina se stavidlem.");
-        public readonly Lokace lesRozcestiDum = new Lokace("Les", "Jsi na lesní cestě, která se klikatí až za obzor, kde mizí v siluetě zapadajícího slunce. Ticho podvečerního lesa občas přeruší zpěv posledních ptáků.");
-        public readonly Lokace dum = new Lokace("Dům", "Stojíš před svým rodným domem, cítíš vůni čerstvě nasekaného dřeva, která se line z hromady vedle vstupních dveří.");
-        private bool konec;
-        private Lokace aktualniLokace;
-        private string[] smery = {"sever", "jih", "východ", "západ"};
+    class Osoba {
+        private string jmeno;
+        private Osoba otec;
+        private Osoba matka;
 
-        public Hra() {
-            hrad.PridejLokaci("východ", lesHradRozcesti);
-            lesHradRozcesti.PridejLokaci("západ", hrad);
-            lesHradRozcesti.PridejLokaci("východ", lesniRozcesti);
-            lesniRozcesti.PridejLokaci("západ", lesHradRozcesti);
-            lesniRozcesti.PridejLokaci("východ", lesRozcestiRybnik);
-            lesniRozcesti.PridejLokaci("jih", lesRozcestiDum);
-            lesRozcestiRybnik.PridejLokaci("západ", lesniRozcesti);
-            lesRozcestiRybnik.PridejLokaci("východ", rybnik);
-            rybnik.PridejLokaci("západ", lesRozcestiRybnik);
-            lesRozcestiDum.PridejLokaci("sever", lesniRozcesti);
-            lesRozcestiDum.PridejLokaci("východ", dum);
-            dum.PridejLokaci("západ", lesRozcestiDum);
-
-            PresunDoJineLokace(dum);
+        public Osoba(string jmeno) {
+            this.jmeno = jmeno;
         }
 
-        public void Akce(string akce) {
-            if (akce == "konec") {
-                konec = true;
-            }
-            else {
-                string[] inputs = akce.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
-                if (inputs.Length != 3) {
-                    Console.WriteLine("Můj vstupní slovník neobsahuje tento příkaz.");
-                } else if (inputs[0] != "jdi" || inputs[1] != "na" || !smery.Contains(inputs[2])) {
-                    Console.WriteLine("Můj vstupní slovník neobsahuje tento příkaz.");
-                }
-                else {
-                    string smer = inputs[2];
-                    if (!aktualniLokace.ObsahujeLokaciSmerem(smer)) {
-                        Console.WriteLine("Tímto směrem nelze jít.");
-                        return;
-                    }
-
-                    Lokace lokace = aktualniLokace.VratLokaciProSmer(smer);
-                    PresunDoJineLokace(lokace);
-                }
-            }
+        public void NastavRodice(Osoba otec, Osoba matka) {
+            this.otec = otec;
+            this.matka = matka;
         }
 
-        public bool Bezi() {
-            return !konec;
+        public Osoba VratOtce() {
+            return otec;
         }
 
-        private void PresunDoJineLokace(Lokace lokace) {
-            aktualniLokace = lokace;
-            Console.WriteLine(lokace.VratNazev());
-            Console.WriteLine(lokace.VratPopis() + "\n");
-            aktualniLokace.VypisMoznosti();
-        }
-    }
-
-    class Lokace {
-        private string nazev;
-        private string popis;
-        private Dictionary<string, Lokace> mapaLokaci;
-
-        public Lokace(string nazev, string popis) {
-            this.nazev = nazev;
-            this.popis = popis;
-            mapaLokaci = new Dictionary<string, Lokace>(2);
+        public Osoba VratMatku() {
+            return matka;
         }
 
-        public void PridejLokaci(string smer, Lokace lokace) {
-            mapaLokaci[smer] = lokace;
-        }
-
-        public bool ObsahujeLokaciSmerem(string smer) {
-            bool naslo;
-            try {
-                naslo = mapaLokaci[smer] != null;
-            }
-            catch (KeyNotFoundException e) {
-                naslo = false;
-            }
-            return naslo;
-        }
-
-        public Lokace VratLokaciProSmer(string smer) {
-            return mapaLokaci[smer];
-        }
-
-        public string VratNazev() {
-            return nazev;
-        }
-
-        public string VratPopis() {
-            return popis;
-        }
-
-        public void VypisMoznosti() {
-            StringBuilder builder = new StringBuilder();
-            builder.Append("Můžeš jít na ");
-            foreach (KeyValuePair<string, Lokace> pair in mapaLokaci) {
-                builder.Append(pair.Key).Append(" ");
-            }
-
-            string moznosti = builder.ToString();
-            moznosti = moznosti.TrimEnd();
-            Console.WriteLine(moznosti);
+        public override string ToString() {
+            return jmeno;
         }
     }
 
